c7f0a4eea941cab6062cc01c13e652dbf40efb07
André Antônio da Silva Queiroz <andreantoniodasilvaqueiroz@gmail.com>
2024-02-03 12:30:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,82 +5,48 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
-const allSongs = [
+const gunsNRosesSongs = [
   {
     id: 0,
-    title: "Scratching The Surface",
-    artist: "Quincy Larson",
-    duration: "4:25",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+    title: "Sweet Child o' Mine",
+    artist: "Guns N' Roses",
+    duration: "5:02",
+    src: "musics/sweetChild.mp3",
   },
   {
     id: 1,
-    title: "Can't Stay Down",
-    artist: "Quincy Larson",
-    duration: "4:15",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
+    title: "November Rain",
+    artist: "Guns N' Roses",
+    duration: "9:16",
+    src: "musics/rain.mp3",
   },
   {
     id: 2,
-    title: "Still Learning",
-    artist: "Quincy Larson",
-    duration: "3:51",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
+    title: "Paradise City",
+    artist: "Guns N' Roses",
+    duration: "6:48",
+    src: "musics/paradise.mp3",
   },
   {
     id: 3,
-    title: "Cruising for a Musing",
-    artist: "Quincy Larson",
-    duration: "3:34",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
+    title: "Welcome to the Jungle",
+    artist: "Guns N' Roses",
+    duration: "4:39",
+    src: "musics/welcome.mp3",
   },
   {
     id: 4,
-    title: "Never Not Favored",
-    artist: "Quincy Larson",
-    duration: "3:35",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
+    title: "Don't Cry",
+    artist: "Guns N' Roses",
+    duration: "5:13",
+    src: "musics/dontCry.mp3",
   },
-  {
-    id: 5,
-    title: "From the Ground Up",
-    artist: "Quincy Larson",
-    duration: "3:12",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
-  },
-  {
-    id: 6,
-    title: "Walking on Air",
-    artist: "Quincy Larson",
-    duration: "3:25",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
-  },
-  {
-    id: 7,
-    title: "Can't Stop Me. Can't Even Slow Me Down.",
-    artist: "Quincy Larson",
-    duration: "3:52",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
-  },
-  {
-    id: 8,
-    title: "The Surest Way Out is Through",
-    artist: "Quincy Larson",
-    duration: "3:10",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
-  },
-  {
-    id: 9,
-    title: "Chasing That Feeling",
-    artist: "Quincy Larson",
-    duration: "2:43",
-    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
-  },
+  
 ];
 
 const audio = new Audio();
 let userData = {
-  songs: [...allSongs],
+  songs: [...gunsNRosesSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
@@ -167,7 +133,7 @@ const deleteSong = (id) => {
     playlistSongs.appendChild(resetButton);
 
     resetButton.addEventListener("click", () => {
-      userData.songs = [...allSongs];
+      userData.songs = [...gunsNRosesSongs];
 
       renderSongs(userData?.songs); 
       setPlayButtonAccessibleText();
